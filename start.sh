b3fe982b704c4c72c7e63cfffffe341444469551
Cauê Thenório <caue@thenorio.com.br>
2020-03-09 00:34:28 -0300

    Adds future script file

---
+++ start.sh
@@ -0,0 +1,7 @@
+# - check if docker is installed
+# - check if docker-compose is installed
+# verify if .env file exists`
+# - verify if gateway docker networks exists:
+#   docker network create gateway
+
+#
